1139c1968a4378bb890a910f4287aaa3880a4688
Mai Schwartz <mai.schwartz@gmail.com>
2017-12-18 15:50:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your solution in this file!
-const driver = { red: 'fin' };
+const driver = { 
+  {
+  name: ['Bob', 'Smith'],
+  age: 32,
+  gender: 'male',
+  interests: ['music', 'skiing']
+}
+
+updateDriverWithKeyAndValue()
